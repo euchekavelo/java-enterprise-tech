c3737d55451ab7b8cc82307c7fdc95f477978af3
Kirill <euchekavelo@gmail.com>
2022-12-06 21:47:09 +0300

    Added changes to the completed tasks of module number 5

--- 05_Microservice_Organization/order-service/src/main/java/ru/skillbox/orderservice/OrderServiceApplication.java
+++ 05_Microservice_Organization/order-service/src/main/java/ru/skillbox/orderservice/OrderServiceApplication.java
@@ -2,8 +2,10 @@ package ru.skillbox.orderservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+@EnableEurekaClient
 @SpringBootApplication
 @EnableJpaRepositories
 public class OrderServiceApplication {
