d254a76ceebf2f890bdea5bb2cc5bf0b14fdbb03
Kirill <euchekavelo@gmail.com>
2022-12-11 14:59:59 +0300

    Added completed tasks for module 8

---
+++ 08_Processes_and_Threads/Processes_and_Threads/src/main/java/Service.java
@@ -0,0 +1,72 @@
+import java.util.LinkedList;
+import java.util.concurrent.locks.ReentrantLock;
+import java.util.stream.IntStream;
+
+public class Service {
+
+    private Integer count = 0;
+    private final LinkedList<String> log = new LinkedList<>();
+
+    public void log() {
+        count = count + 1;
+        String threadName = Thread.currentThread().getName();
+        log.addLast(threadName);
+    }
+
+    public void debug() {
+        System.out.println(count);
+        log.forEach(System.out::println);
+    }
+
+    public static void main(String[] args) throws Exception {
+        callServiceClassMethods();
+    }
+
+    private static void callServiceClassMethods() throws Exception {
+        Service service = new Service();
+        ReentrantLock lock = new ReentrantLock();
+
+        Thread t1 = new Thread(() -> {
+            lock.lock();
+            try {
+                IntStream.range(0, 10).forEach(i -> service.log());
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            } finally {
+                lock.unlock();
+            }
+        }, "T1");
+
+        Thread t2 = new Thread(() -> {
+            lock.lock();
+            try {
+                IntStream.range(0, 10).forEach(i -> service.log());
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            } finally {
+                lock.unlock();
+            }
+        }, "T2");
+
+        Thread t3 = new Thread(() -> {
+            lock.lock();
+            try {
+                IntStream.range(0, 10).forEach(i -> service.log());
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            } finally {
+                lock.unlock();
+            }
+        }, "T3");
+
+        t1.start();
+        t2.start();
+        t3.start();
+
+        t1.join();
+        t2.join();
+        t3.join();
+
+        service.debug();
+    }
+}
