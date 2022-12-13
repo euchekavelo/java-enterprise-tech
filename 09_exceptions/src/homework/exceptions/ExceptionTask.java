dc2b05468f21a13b66f50a1d528384c105d41359
Kirill <euchekavelo@gmail.com>
2022-12-13 23:08:05 +0300

    Added a project with completed tasks of module 9

--- 09_exceptions/src/homework/exceptions/ExceptionTask.java
+++ 09_exceptions/src/homework/exceptions/ExceptionTask.java
@@ -5,26 +5,48 @@ import java.util.Optional;
 public class ExceptionTask {
 
     public static Optional<String> mergeStrings(String first, String second) {
+        if (first == null) {
+            first = "";
+        }
+
+        if (second == null) {
+            second = "";
+        }
+
+        if (first.isEmpty() && second.isEmpty()) {
+            return Optional.empty();
+        }
+
         return Optional.of(first.length() > second.length() ? first + second : second + first);
     }
 
-    public static long getPower(int n, int p) throws Exception {
-        if (n < 0 || p < 0) {
-            throw new Exception();
-        } else if (n == 0 && p == 0) {
-            throw new Exception();
+    public static long getPower(int n, int p) {
+        if ((n < 0 || p < 0) || n == 0) {
+            return -1L;
         } else {
             return (long) (Math.pow(n, p));
         }
     }
 
     public static void customException(int a) {
-        throwUncheckedException();
+        if (a == 0) {
+            throw new InvalidZeroParameterException();
+        }
+
+        try {
+            throwUncheckedException();
+        } catch (RuntimeException ex) {
+            ex.printStackTrace();
+        }
     }
 
-    public static void exceptionProcessing() throws Exception {
-        throwCheckedException();
-        throwUncheckedException();
+    public static void exceptionProcessing() {
+        try {
+            throwCheckedException();
+            throwUncheckedException();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     private static void throwCheckedException() throws Exception {
