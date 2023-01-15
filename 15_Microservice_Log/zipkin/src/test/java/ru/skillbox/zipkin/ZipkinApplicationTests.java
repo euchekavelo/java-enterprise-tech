c76423053072fcb66b8b0878ab7531fde5dbb43a
Kirill <euchekavelo@gmail.com>
2023-01-15 04:47:14 +0300

    Added a project with completed tasks of module 15

---
+++ 15_Microservice_Log/zipkin/src/test/java/ru/skillbox/zipkin/ZipkinApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.skillbox.zipkin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZipkinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
